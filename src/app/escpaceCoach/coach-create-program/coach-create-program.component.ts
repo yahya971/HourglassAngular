f4546f5a8807f6dcdd5dca67fd28cdf87e1a2e47
ahmedhajyahmed <hajyahmedboh@yahoo.com>
2019-12-21 16:46:35 +0100

    -

--- src/app/escpaceCoach/coach-create-program/coach-create-program.component.ts
+++ src/app/escpaceCoach/coach-create-program/coach-create-program.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-coach-create-program',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CoachCreateProgramComponent implements OnInit {
 
-  constructor() { }
+  id: any;
+  constructor(private router: ActivatedRoute) {
+    this.router.params.subscribe(value => {
+      this.id = value.id;
+      });
+  }
 
   ngOnInit() {
   }
