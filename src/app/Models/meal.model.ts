bad883e248e27e8441a22928294fe9ace9066942
ahmedhajyahmed <hajyahmedboh@yahoo.com>
2019-12-27 17:23:03 +0100

    my meals (back)

--- src/app/Models/meal.model.ts
+++ src/app/Models/meal.model.ts
@@ -1,9 +1,13 @@
 export class Meal {
   id: number;
+  name: string;
   caloricValue: number;
   photo: Array<any>;
-  isTaken: Boolean;
+  isTaken: boolean;
   recipe: string;
+  description: string;
   ingredients: string;
+  type: string;
+  time: string;
   coach: any;
 }
--- src/app/services/meal.service.ts
+++ src/app/services/meal.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import {Meal} from '../Models/meal.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,5 +13,9 @@ export class MealService {
 
   getMealById(id) {
 
+  }
+
+  getMealByCoachId(id) {
+    return this.http.get<Array<Meal>>('http://localhost:8080/meal/byCoach/' + id);
   }
 }
